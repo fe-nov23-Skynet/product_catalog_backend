0dfc9095a859454a9c3b507cdeea279c05631732
Yaroslav Samoilenko <yasik2255@live.com>
2024-03-24 15:30:16 +0200

    realized products api get

---
+++ pages/api/accessories.js
@@ -0,0 +1,19 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+
+export default async function handle(req, res) {
+  const { method } = req;
+  await mongooseConnect()
+
+  if (method === "GET") {
+      if (req.query?.id) {
+          res.json(await Product.findOne({ id: req.query.id }))
+      } else {
+        try {
+          res.json(await Product.find({ category: "accessories" }))
+        } catch (error) {
+          res.status(404).json([]);
+        }
+      }
+  }
+};
